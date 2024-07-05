a81a9656485ae7de83120905f6ff695324880586
albertoluques <157401388+albertoluques@users.noreply.github.com>
2024-07-05 15:09:21 -0700

    Error treatment and Validatation

--- src/main/java/com/forum_hub/forum/infra/errors/ErrorTreatment.java
+++ src/main/java/com/forum_hub/forum/infra/errors/ErrorTreatment.java
@@ -1,2 +1,40 @@
-package com.forum_hub.forum.infra.errors;public class ErrorTreatment {
+package com.forum_hub.forum.infra.errors;
+
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.ValidationException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ErrorTreatment {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity tratarError404() {
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity tratarError400(MethodArgumentNotValidException e) {
+        var errores = e.getFieldErrors().stream().map(DatosErrorValidacion::new).toList();
+        return ResponseEntity.badRequest().body(errores);
+    }
+
+    @ExceptionHandler(IntegrityValidation.class)
+    public ResponseEntity errorHandlerIntegrityValidation(Exception e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
+
+    @ExceptionHandler(ValidationException.class)
+    public ResponseEntity errorHandlerValidacionesDeNegocio(Exception e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
+
+    private record DatosErrorValidacion(String text, String error) {
+        public DatosErrorValidacion(FieldError error) {
+            this(error.getField(), error.getDefaultMessage());
+        }
+    }
 }
--- src/main/java/com/forum_hub/forum/infra/errors/IntegrityValidation.java
+++ src/main/java/com/forum_hub/forum/infra/errors/IntegrityValidation.java
@@ -1,2 +1,7 @@
-package com.forum_hub.forum.infra.errors;public class IntegrityValidation {
+package com.forum_hub.forum.infra.errors;
+
+public class IntegrityValidation extends RuntimeException {
+    public IntegrityValidation(String s) {
+        super(s);
+    }
 }
